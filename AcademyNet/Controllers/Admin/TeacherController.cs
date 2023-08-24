d80b4ef7bed81a38a7181790f47bcf97d239ae4f
f.momen <f.momen@karestan.com>
2023-08-25 02:43:53 +0430

    #SolveConflict

--- AcademyNet/Controllers/Admin/TeacherController.cs
+++ AcademyNet/Controllers/Admin/TeacherController.cs
@@ -7,10 +7,10 @@ namespace AcademyNet.Controllers.Admin
 {
     public class TeacherController : Controller
     {
-        private  IWebHostEnvironment webHostEnvironment;
+        private IWebHostEnvironment webHostEnvironment;
         public TeacherController(IWebHostEnvironment _webHostEnvironment)
         {
-            webHostEnvironment=_webHostEnvironment;
+            webHostEnvironment = _webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -20,6 +20,27 @@ namespace AcademyNet.Controllers.Admin
         public IActionResult Create()
         {
             return View("Create");
+        }
+        [HttpPost]
+        public void Create(Models.Human human)
+        {
+            BLLHuman bLLHuman = new BLLHuman();
+
+            Business_Entity.Human businessHuman = new Human();
+            businessHuman.Name = human.Name;
+            businessHuman.Family = human.Family;
+            businessHuman.Email = human.Email;
+            businessHuman.gender = (Human.Gender)human.gender;
+            businessHuman.Password = human.Password;
+            businessHuman.role = (Human.Role)human.role;
+            UploadFile uploadFile = new UploadFile(webHostEnvironment);
+            if (human.Picture != null)
+            {
+                businessHuman.Picture = uploadFile.Upload(human.Picture);
+            }
+            bLLHuman.Create(businessHuman);
+
+
         }
     }
 }
